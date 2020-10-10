8602d9bda748ad0eb1422ee603401b5ec8711975
JAkutenshi <jakutenshi@gmail.com>
2020-10-10 15:07:07 +0300

    Davydov's github added to fetch scripts and README.md

--- fetch.sh
+++ fetch.sh
@@ -2,7 +2,7 @@ git fetch dmnistratov     # Нистратов
 git fetch KonstantM       # Мосин
 git fetch zipzap666       # Звега
 git fetch sosedkov-kirill # Соседков
-#git fetch                # 
+git fetch Dmitrydavka     # Давыдов
 #git fetch                # 
 #git fetch                # 
 #git fetch                # 
--- remotes.sh
+++ remotes.sh
@@ -2,29 +2,22 @@ git remote add dmnistratov               https://github.com/dmnistratov/spbetu_a
 git remote add KonstantM                 https://github.com/KonstantM/spbetu_aads_2020_9384.git              # Мосин
 git remote add zipzap666                 https://github.com/zipzap666/spbetu_aads_2020_9384.git              # Звега
 git remote add sosedkov-kirill           https://github.com/sosedkov-kirill/spbetu_aads_2020_9384.git        # Соседков
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+git remote add Dmitrydavka               https://github.com/Dmitrydavka/spbetu_aads_2020_9384.git            # Давыдов
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
+#git remote add                           https://github.com//spbetu_aads_2020_9384.git                       # 
