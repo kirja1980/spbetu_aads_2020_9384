a350f18ac64b8b4b52aec9977caf0f1f93380bb3
JAkutenshi <jakutenshi@gmail.com>
2020-10-10 11:57:03 +0300

    Fetch scripts are created

---
+++ fetch.sh
@@ -0,0 +1,20 @@
+git fetch dmnistratov     # Нистратов
+git fetch KonstantM       # Мосин
+git fetch zipzap666       # Звега
+git fetch sosedkov-kirill # Соседков
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
+#git fetch                # 
---
+++ remotes.sh
@@ -0,0 +1,30 @@
+git remote add dmnistratov               https://github.com/dmnistratov/spbetu_aads_2020_9384.git            # Нистратов
+git remote add KonstantM                 https://github.com/KonstantM/spbetu_aads_2020_9384.git              # Мосин
+git remote add zipzap666                 https://github.com/zipzap666/spbetu_aads_2020_9384.git              # Звега
+git remote add sosedkov-kirill           https://github.com/sosedkov-kirill/spbetu_aads_2020_9384.git        # Соседков
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
