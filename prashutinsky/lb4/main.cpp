c5cf5362bf435a6609ff62d2caaf9154238b8fcb
kirja1980 <55086970+kirja1980@users.noreply.github.com>
2020-11-19 22:24:57 +0300

    Prashutinsky lb4 redone

--- prashutinsky/lb4/main.cpp
+++ prashutinsky/lb4/main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "MergeSort.h"
 
-void main()
+template<typename base>
+void Merge(base *A, int first, int last)
+{
+	int middle = 0, start = 0, final = 0;
+	int n = last - first + 1;
+	base *mas = new base[n];
+	middle = (first + last) / 2;
+	start = first;
+	final = middle + 1;
+	for (int j = first; j <= last; j++)
+		if ((start <= middle) && ((final > last) || (A[start] < A[final])))
+		{
+			mas[j - first] = A[start];
+			start++;
+		}
+		else
+		{
+			mas[j - first] = A[final];
+			final++;
+		}
+
+	for (int j = first; j <= last; j++) A[j] = mas[j - first];
+	delete[]mas;
+}
+
+template<typename base>
+void MergeSort(base *A, int first, int last)
+{
+	if (first < last)
+	{
+		MergeSort(A, first, (first + last) / 2);
+		MergeSort(A, (first + last) / 2 + 1, last);
+		Merge(A, first, last);
+	}
+}
+
+int main()
 {
 	setlocale(LC_ALL, "Rus");
 
@@ -66,4 +101,5 @@ void main()
 		}
 	}
 	system("pause");
+	return 0;
 }
